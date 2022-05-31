4d51ce757aab45708ade4339b6a3c314852c35a9
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-05-31 23:37:46 +0800

    init

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,32 +1,38 @@
 <?php
-if(!defined('DB_SERVER')){
+if (!defined('DB_SERVER')) {
     require_once("../initialize.php");
 }
-class DBConnection{
+class DBConnection
+{
 
     private $host = DB_SERVER;
     private $username = DB_USERNAME;
     private $password = DB_PASSWORD;
     private $database = DB_NAME;
     private $port = DB_PORT;
-    
+
     public $conn;
-    
-    public function __construct(){
+
+    public function __construct()
+    {
 
         if (!isset($this->conn)) {
-            
+
             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
-            
+
+            /* check connection */
+            if ($this->conn->connect_errno) {
+                echo("Connect failed: %s\n",$this->conn->connect_error);
+                exit();
+            }
             if (!$this->conn) {
                 echo 'Cannot connect to database server';
                 exit;
-            }            
-        }    
-        
+            }
+        }
     }
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->conn->close();
     }
 }
-?>
--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','https://casanas.io/www/remittance/');
+if(!defined('base_url')) define('base_url','https://casanas.io/remittance/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
@@ -9,3 +9,4 @@ if(!defined('DB_PORT')) define('DB_PORT',"3306");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
 if(!defined('DB_NAME')) define('DB_NAME',"remittance_db");
 ?>
+
